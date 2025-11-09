9c038cea6528ff294da7212bbe82132cf73dcaeb
MarcoTulioGT <devopsgt@gmail.com>
2025-11-09 16:16:03 -0600

    react

--- store_tbt/src/App.tsx
+++ store_tbt/src/App.tsx
@@ -596,7 +596,7 @@ const App: React.FC = () => {
           </div>
         </div>
         <div className="border-t border-white/10 py-6 text-center text-sm text-neutral-400">
-          © {new Date().getFullYear()} TheBrothersToys. All rights reserved.
+          © {new Date().getFullYear()} The Brothers Toys. All rights reserved.
         </div>
       </footer>
     </div>
